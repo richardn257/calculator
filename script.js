87f332b085c44bfab558c80943fb9b4411c774a3
Richard Nguyen <nguyenrichard972@gmail.com>
2022-08-19 15:02:39 -0700

    Edit the display function where cannot divide by 0. Need some more work. Start the number with a nonzero, only one decimal, and backspace button.

--- script.js
+++ script.js
@@ -21,7 +21,10 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b != '0')
+        return a / b;
+    alert("You can't divide by 0")
+    return a;
 }
 
 function operate(op, a, b) {
@@ -36,8 +39,7 @@ function operate(op, a, b) {
 }
 
 let num1 = "", num2 = "", op = "";
-const operators = ['+', '-', '×', '÷'];
-let oneOp = false;
+let oneOp = false, displayOp = true;
 
 function display(btnVal) {
     if (btnVal == 'DELETE' || btnVal == 'CLEAR') {
@@ -46,30 +48,44 @@ function display(btnVal) {
         num2 = "";
         op = "";
         moveToNum2 = false;
+        oneOp = false;
     }
     else {
         if ((btnVal == '+' || btnVal == '-' || btnVal == '×' || btnVal == '÷' || btnVal == '=') && num2 != "") {
             num1 = operate(op, +num1, +num2);
             screen.textContent = num1;
+
+            if (num2 != '0') {
+                op = btnVal;
+                if (btnVal != '=')
+                    screen.textContent += btnVal;
+            }
+            else {
+                screen.textContent += op;
+            }
+
             num2 = "";
-            op = btnVal;
         }
         else if ((btnVal == '+' || btnVal == '-' || btnVal == '×' || btnVal == '÷' || btnVal == '=')) {
             if (btnVal != '=' && num1 != '' && !oneOp) {
                 op = btnVal;
                 oneOp = true;
+                screen.textContent += btnVal
             }
+        }
+        else if (op == '=') {
+            num1 = btnVal;
+            screen.textContent = btnVal;
+            op = '';
+            oneOp = false;
         }
         else if (op == '') {
             num1 += btnVal;
-        } 
+            screen.textContent += btnVal
+        }
         else {
             num2 += btnVal;
+            screen.textContent += btnVal
         }
-
-        if (btnVal != '=' && num1 != '')
-            screen.textContent += btnVal;
     }
 }
-
-// console.log(operate("/", 6, 2))
